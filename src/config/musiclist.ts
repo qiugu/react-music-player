d54b3d2ff91f04d3259f606c8d84f7df236426d0
qiugu <476710564@qq.com>
2021-07-29 20:09:27 +0800

    refactor: change static js file to ts

--- src/config/musiclist.ts
+++ src/config/musiclist.ts
@@ -4,42 +4,35 @@ const MUSIC_LIST = [
         title: '表演时间',
         artist: '本兮',
         file: 'http://www.ytmp3.cn/down/49265.mp3',
-        cover: 'http://q1.qlogo.cn/g?b=qq&nk=1239158377&s=100&t=1626872603'
+        cover: 'https://img01.dmhmusic.com/0206/M00/2A/89/ChR461zOROaAU7_oACi2_scz7KI564.jpg@w_300,h_300'
     },
     {
         id: 2,
         title: '讲真的',
         artist: '涂涂',
         file: 'http://www.ytmp3.cn/down/49275.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/87.jpg'
+        cover: 'https://img01.dmhmusic.com/0207/M00/64/4A/ChR461sY3o6AZpu2ADP_kW9TIHM494.jpg@w_300,h_300'
     },
     {
         id: 3,
         title: '素颜',
         artist: '许嵩&何曼婷',
         file: 'http://www.ytmp3.cn/down/49271.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/17.jpg'
-    },
-    {
-        id: 4,
-        title: '念念不忘',
-        artist: '杨凯莉',
-        file: 'http://www.ytmp3.cn/down/49233.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/37.jpg'
+        cover: 'https://img01.dmhmusic.com/0104/M00/62/15/ChR45WDxc4GAGs3cAACq1knkE1s086.jpg@w_300,h_300'
     },
     {
         id: 5,
         title: '海草舞',
         artist: '萧全',
         file: 'http://www.ytmp3.cn/down/49159.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/50.jpg'
+        cover: 'https://img01.dmhmusic.com/0208/M00/67/AB/ChR47FshUpGAGhfpAAD23SUyvNI569.jpg@w_300,h_300'
     },
     {
         id: 6,
         title: 'Mad',
         artist: 'Clown&金娜英',
         file: 'http://www.ytmp3.cn/down/48959.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/83.jpg'
+        cover: 'https://img01.dmhmusic.com/0412/M00/97/2A/ChAKEmBko0uAI4nZAALn3PJAk1c633.jpg@w_300,h_300'
     }
 ];
 // import $ from "jquery";
