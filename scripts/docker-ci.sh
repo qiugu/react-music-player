b1d259e5802205348405cfb9808368bbb5b2a8a9
qiugu <476710564@qq.com>
2023-03-11 23:06:14 +0800

    chore: build scripts update

--- scripts/docker-ci.sh
+++ scripts/docker-ci.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-
-# 初始缓存下载的依赖
-docker build --target dependency --build-arg BUILDKIT_INLINE_CACHE=1 -t dep ../
-docker tag dep qiugu/dep:2.0
-docker image ls
-docker push qiugu/dep:2.0
-
-# 后续更新使用依赖缓存
-# docker build --cache-from qiugu/dep:1.0 -t music ..
-# docker image ls
-# docker run -dp 8003:8003 --name demo music
+base_image=qiugu/dep:2.0;
+music_image=qiugu/music:1.0;
+docker pull $base_image || true;
+docker pull $music_image || true;
+docker build -t $music_image --cache-from $base_image --cache-from $music_image --build-arg BUILDKIT_INLINE_CACHE=1 ../;
+docker push $base_image;
+docker push $music_image;
