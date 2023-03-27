bfa5d58a06aaf1bc12612dc2e450d2e42f43f166
qiugu <476710564@qq.com>
2023-03-27 22:30:28 +0800

    chore: Add build arg option

--- scripts/docker-ci.sh
+++ scripts/docker-ci.sh
@@ -3,6 +3,7 @@ base_image=qiugu/dep;
 music_image=qiugu/music;
 docker pull $base_image || true;
 docker pull $music_image || true;
-docker build -t $music_image --cache-from $base_image --cache-from $music_image --build-arg BUILDKIT_INLINE_CACHE=1 ../;
+# docker build -t $music_image --cache-from $base_image --cache-from $music_image --build-arg BUILDKIT_INLINE_CACHE=1 ../;
+docker build -t $music_image ../;
 docker push $base_image;
 docker push $music_image;
