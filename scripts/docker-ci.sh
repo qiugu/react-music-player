07189e1cbd9e0bc8a901768b2c802dba389b7445
qiugu <476710564@qq.com>
2023-03-27 21:44:57 +0800

    chore: set cache option7

--- scripts/docker-ci.sh
+++ scripts/docker-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-base_image=qiugu/dep:2.0;
-music_image=qiugu/music:1.0;
+base_image=qiugu/dep;
+music_image=qiugu/music;
 docker pull $base_image || true;
 docker pull $music_image || true;
 docker build -t $music_image --cache-from $base_image --cache-from $music_image --build-arg BUILDKIT_INLINE_CACHE=1 ../;
